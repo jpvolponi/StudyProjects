c70ac2ff108a70a729b6d7231eb65735d451a4be
jpvolponi <pierre.volponi@gmail.com>
2023-05-15 23:48:30 -0300

    package models update

--- ToDoAPI/models/delete.go
+++ ToDoAPI/models/delete.go
@@ -1,15 +1,17 @@
 package models
 
-fun Update (id int64) (int64, error) {
+import "StudyProjects/ToDoAPI/db"
+
+func Delete(id int64) (int64, error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
-		retunr 0, err
+		return 0, err
 	}
 	defer conn.Close()
 
-	res, err := conn.Exec(`DELETE FROM todos WHERE id=$4`, id)
+	res, err := conn.Exec(`DELETE FROM todos WHERE id=$1`, id)
 	if err != nil {
-		retunr 0, err
+		return 0, err
 	}
 
 	return res.RowsAffected()
--- ToDoAPI/models/update.go
+++ ToDoAPI/models/update.go
@@ -1,11 +1,18 @@
 package models
 
-fun Update (id int64, todo Todo) (int64, error) {
+import "StudyProjects/ToDoAPI/db"
+
+func Update(id int64, todo Todo) (int64, error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
-		retunr 0, err
+		return 0, err
 	}
 	defer conn.Close()
 
 	res, err := conn.Exec(`UPDATE todos SET title=$1, description=$2, done=$3 WHERE id=$4`, todo.Title, todo.Description, todo.Done, id)
+	if err != nil {
+		return 0, err
+	}
+
+	return res.RowsAffected()
 }
