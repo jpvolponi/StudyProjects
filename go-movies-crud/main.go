c8e96f202eb951b316c7e8a911490bcdfb82ad00
jpvolponi <pierre.volponi@gmail.com>
2023-06-24 00:48:10 -0300

    new project

---
+++ go-movies-crud/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `jason:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+}
